4fd7961fcb9462a0181c4947193b730710dfe374
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-21 19:42:54 -0300

    🔧 chore: add eslint

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,9 @@ import express from 'express';
 // https://expressjs.com/pt-br/
 const app = express();
 
-app.get('/test', (req, res) => {
-  return res.send('Hello World!');
-});
+app.get('/test', (req, res) => res.send('Hello World!'));
 
-app.post('/test-post', (req, res) => {
-  return res.send('Hello World, POST successful!');
-});
+app.post('/test-post', (req, res) => res.send('Hello World, POST successful!'));
 
 // Listening on localhost:3000
 app.listen(3000, () => console.log('Server is running!'));

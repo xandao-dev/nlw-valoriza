ff1f3d907862c15f45fd5088fa57a9126433b3cd
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-26 16:15:27 -0300

    ✨ feat(compliment): add controller

---
+++ src/controllers/CreateComplimentController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import CreateComplimentService from '../services/CreateComplimentService';
+
+export default class CreateComplimentController {
+	async handle(request: Request, response: Response) {
+		const { tagId, userSender, userReceiver, message } = request.body;
+		const createComplimentService = new CreateComplimentService();
+		const compliment = await createComplimentService.execute({
+			tagId,
+			userSender,
+			userReceiver,
+			message,
+		});
+		return response.json(compliment);
+	}
+}
--- src/services/CreateComplimentService.ts
+++ src/services/CreateComplimentService.ts
@@ -3,31 +3,31 @@ import ComplimentRepository from '../repositories/ComplimentRepository';
 import UserRepository from '../repositories/UserRepository';
 
 interface IComplimentRequest {
-	tag_id: string;
-	user_sender: string;
-	user_receiver: string;
+	tagId: string;
+	userSender: string;
+	userReceiver: string;
 	message: string;
 }
 export default class CreateComplimentService {
-	async execute({ tag_id, user_sender, user_receiver, message }: IComplimentRequest) {
+	async execute({ tagId, userSender, userReceiver, message }: IComplimentRequest) {
 		const complimentRepository = getCustomRepository(ComplimentRepository);
 		const userRepository = getCustomRepository(UserRepository);
 
 		// Check if user receiver is different from user sender
-		if (user_sender === user_receiver) {
+		if (userSender === userReceiver) {
 			throw new Error('Incorrect User Receiver');
 		}
 
 		// Check if user receiver exists in db
-		const userReceiverExists = await userRepository.findOne(user_receiver);
+		const userReceiverExists = await userRepository.findOne(userReceiver);
 		if (!userReceiverExists) {
 			throw new Error('User Receiver does not exists!');
 		}
 
 		const compliment = complimentRepository.create({
-			tag_id,
-			user_receiver,
-			user_sender,
+			tag_id: tagId,
+			user_sender: userSender,
+			user_receiver: userReceiver,
 			message,
 		});
 

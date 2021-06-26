2426b24bfdbd546a943f02b0a13a495a4c9c0fdd
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-26 14:45:25 -0300

    ✨ feat(login): add jwt

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import ensureAdmin from './middlewares/ensureAdmin';
 import CreateUserController from './controllers/CreateUserController';
 import CreateTagController from './controllers/CreateTagController';
+import AuthenticateUserController from './controllers/AuthenticateUserController';
 
 const router = Router();
+const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 
+router.post('/login', authenticateUserController.handle);
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAdmin, createTagController.handle);
 

7d28471049cc807ff36822dfeb65b524c740624f
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-26 09:45:53 -0300

    ✨ feat(tag): add creation route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import CreateUserController from './controllers/CreateUserController';
+import CreateTagController from './controllers/CreateTagController';
 
 const router = Router();
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post('/users', createUserController.handle);
+router.post('/tags', createTagController.handle);
 
 export default router;

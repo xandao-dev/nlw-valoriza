bb9077b4db8173fdc21bf5ae617fd903703845cb
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-26 16:18:39 -0300

    ✨ feat(compliment): add route

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import ensureAdmin from './middlewares/ensureAdmin';
+import AuthenticateUserController from './controllers/AuthenticateUserController';
 import CreateUserController from './controllers/CreateUserController';
 import CreateTagController from './controllers/CreateTagController';
-import AuthenticateUserController from './controllers/AuthenticateUserController';
+import CreateComplimentController from './controllers/CreateComplimentController';
 
 const router = Router();
 const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
 
 router.post('/login', authenticateUserController.handle);
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAdmin, createTagController.handle);
+router.post('/compliments', createComplimentController.handle);
 
 export default router;

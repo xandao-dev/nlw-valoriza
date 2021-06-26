3b3137c3aed2eba31ccd3a8d021504521994500e
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-26 15:40:19 -0300

    🐛 fix(compliment): missing properties in entity

--- src/entities/Compliment.ts
+++ src/entities/Compliment.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryColumn, CreateDateColumn, JoinColumn, ManyToOne } from 'typeorm';
+import { Entity, PrimaryColumn, CreateDateColumn, JoinColumn, ManyToOne, Column } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import Tag from './Tag';
 import User from './User';
@@ -8,17 +8,29 @@ export default class Compliment {
 	@PrimaryColumn()
 	readonly id: string;
 
+	@Column()
+	user_sender: string;
+
 	@JoinColumn({ name: 'user_sender' })
 	@ManyToOne(() => User)
 	userSender: User;
+
+	@Column()
+	user_receiver: string;
 
 	@JoinColumn({ name: 'user_receiver' })
 	@ManyToOne(() => User)
 	userReceiver: User;
 
+	@Column()
+	tag_id: string;
+
 	@JoinColumn({ name: 'tag_id' })
 	@ManyToOne(() => Tag)
 	tag: Tag;
+
+	@Column()
+	message: string;
 
 	@CreateDateColumn()
 	created_at: Date;

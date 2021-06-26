212251c156ea71090423b130508c7855608fa031
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-25 23:20:35 -0300

    🐛 fix: handle error with middlewares (#1)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import './database';
-import express from 'express';
+import express, { Request, Response, NextFunction, response } from 'express';
+import 'express-async-errors';
 import router from './routes';
 
 // https://expressjs.com/pt-br/
@@ -8,6 +9,17 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof Error) {
+		return res.status(400).json({
+			error: err.message,
+		});
+	}
+	return res.status(500).json({
+		status: 'error',
+		message: 'Internal Server Error',
+	});
+});
 
 // Listening on localhost:3000
 app.listen(3000, () => console.log('Server is running!'));

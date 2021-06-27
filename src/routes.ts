aa7212b5a80973dbd5e7a7583867bdeba2fe94fb
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-26 22:45:19 -0300

    ✨feat(tag): list tags (#11)
    * ✨ feat(tag): add list service
    
    * ✨ feat(tag): add list controller
    
    * ✨ feat(tag): add list route
    
    * 🐛 fix(tag): missing parameter in listTagsController

--- src/routes.ts
+++ src/routes.ts
@@ -7,20 +7,23 @@ import CreateTagController from './controllers/CreateTagController';
 import CreateComplimentController from './controllers/CreateComplimentController';
 import ListUserSendComplimentsController from './controllers/ListUserSendComplimentsController';
 import ListUserReceiveComplimentsController from './controllers/ListUserReceiveComplimentsController';
+import ListTagsController from './controllers/ListTagsController';
 
 const router = Router();
 const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
-const createTagController = new CreateTagController();
-const createComplimentController = new CreateComplimentController();
 const listUserSendComplimentsController = new ListUserSendComplimentsController();
 const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
+const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
+const listTagsController = new ListTagsController();
 
 router.post('/login', authenticateUserController.handle);
 router.post('/users', createUserController.handle);
-router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
-router.post('/compliments', ensureAuthenticated, createComplimentController.handle);
 router.get('/user/compliments/sent', ensureAuthenticated, listUserSendComplimentsController.handle);
 router.get('/user/compliments/received', ensureAuthenticated, listUserReceiveComplimentsController.handle);
+router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
+router.get('/tags', ensureAuthenticated, listTagsController.handle);
+router.post('/compliments', ensureAuthenticated, createComplimentController.handle);
 
 export default router;

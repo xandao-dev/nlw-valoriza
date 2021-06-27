572f75aec9a128dd7fbb43d1fd89b9025f12d832
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-26 23:46:30 -0300

    ♻️ refactor(user): remove user password from listing using class-transformer

--- src/services/ListUsersService.ts
+++ src/services/ListUsersService.ts
@@ -1,15 +1,11 @@
 import { getCustomRepository } from 'typeorm';
+import { classToPlain } from 'class-transformer';
 import UserRepository from '../repositories/UserRepository';
 
 export default class ListUserReceiveComplimentsService {
 	async execute() {
 		const userRepository = getCustomRepository(UserRepository);
 		const users = await userRepository.find();
-		const usersWithoutPassword = users.map((user) => {
-			const { password, ...userInfo } = user;
-			return userInfo;
-		});
-
-		return usersWithoutPassword;
+		return classToPlain(users);
 	}
 }

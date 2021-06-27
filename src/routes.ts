73016f0e6cd7976a590e40aa08a51fae8b4eb3e5
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-27 16:37:29 -0300

    ✨ feat(user): add user update (#15)
    * 🔧 chore(user): add default value to admin
    
    * 🐛 fix(user): do not allow to create admin users
    
    * ✨ feat(user): add service to update user
    
    * ✨ feat(user): add controller to update user
    
    * ✨ feat(user): add route to update user

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import AuthenticateUserController from './controllers/AuthenticateUserController
 import CreateUserController from './controllers/CreateUserController';
 import CreateTagController from './controllers/CreateTagController';
 import CreateComplimentController from './controllers/CreateComplimentController';
+import AdminUpdateUserController from './controllers/AdminUpdateUserController';
 import ListUserSendComplimentsController from './controllers/ListUserSendComplimentsController';
 import ListUserReceiveComplimentsController from './controllers/ListUserReceiveComplimentsController';
 import ListTagsController from './controllers/ListTagsController';
@@ -19,9 +20,11 @@ const listUserReceiveComplimentsController = new ListUserReceiveComplimentsContr
 const createTagController = new CreateTagController();
 const createComplimentController = new CreateComplimentController();
 const listTagsController = new ListTagsController();
+const adminUpdateUserController = new AdminUpdateUserController();
 
 router.post('/login', authenticateUserController.handle);
 router.post('/users', createUserController.handle);
+router.put('/users', ensureAuthenticated, ensureAdmin, adminUpdateUserController.handle);
 router.get('/users', ensureAuthenticated, ensureAdmin, listUsersController.handle);
 router.get('/user/compliments/sent', ensureAuthenticated, listUserSendComplimentsController.handle);
 router.get('/user/compliments/received', ensureAuthenticated, listUserReceiveComplimentsController.handle);

e76359b3403755c396b7740ac4a678e28111bbc5
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-26 23:08:29 -0300

    ✨ feat(tag): add class transformer to show custom field (#12)

--- src/services/ListTagsService.ts
+++ src/services/ListTagsService.ts
@@ -1,10 +1,11 @@
 import { getCustomRepository } from 'typeorm';
+import { classToPlain } from 'class-transformer';
 import TagRepository from '../repositories/TagRepository';
 
 export default class ListTagsService {
 	async execute() {
 		const tagRepository = getCustomRepository(TagRepository);
 		const tags = await tagRepository.find();
-		return tags;
+		return classToPlain(tags);
 	}
 }

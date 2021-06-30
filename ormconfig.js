737056bb431736be4452ada75665bdc129584e97
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-06-30 13:42:34 -0300

    👷 build(docker): docker for production and development!
    * 🔧 chore: update environment variables
    
    * 👷 build: add production scripts
    
    * 👷 build(docker): docker for production and development!

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,21 @@
+let baseDir = '';
+if (process.env.NODE_ENV === 'development') {
+	baseDir = 'src';
+} else if (process.env.NODE_ENV === 'production') {
+	baseDir = 'dist';
+}
+
 module.exports = {
 	type: 'postgres',
-	host: 'localhost',
-	port: 5432,
+	host: process.env.DATABASE_LINK || 'localhost',
 	username: process.env.DATABASE_USERNAME,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
-	migrations: ['src/database/migrations/*.ts'],
-	entities: ['src/entities/*.ts'],
+	port: process.env.DATABASE_HOST_PORT,
+	migrations: [`${baseDir}/database/migrations/*{.ts,.js}`],
+	entities: [`${baseDir}/entities/*{.ts,.js}`],
 	cli: {
-		migrationsDir: 'src/database/migrations',
-		entitiesDir: 'src/entities',
+		migrationsDir: `${baseDir}/database/migrations`,
+		entitiesDir: `${baseDir}/entities`,
 	},
 };

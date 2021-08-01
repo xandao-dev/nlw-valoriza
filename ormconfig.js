ad911abe783205f9b4567e44a4711deae0230fa6
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-08-01 15:36:33 -0300

    chore: add driver to connect to aurora serverless RDS.

--- ormconfig.js
+++ ormconfig.js
@@ -6,12 +6,15 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 module.exports = {
-	type: 'postgres',
+	type: process.env.DATABASE_CON_TYPE,
 	host: process.env.DATABASE_LINK || 'localhost',
 	username: process.env.DATABASE_USERNAME,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
 	port: process.env.DATABASE_HOST_PORT,
+	secretArn: process.env.AWS_SECRET_ARN,
+	resourceArn: process.env.AWS_RESOURCE_ARN,
+	region: process.env.AWS_REGION,
 	migrations: [`${baseDir}/database/migrations/*{.ts,.js}`],
 	entities: [`${baseDir}/entities/*{.ts,.js}`],
 	cli: {
